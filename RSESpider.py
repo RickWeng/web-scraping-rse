fc80032a9f6f9fe896ce8b4b4f227cc0b611456e
Ricky Weng <ricky.weng@canada.ca>
2019-06-05 23:21:30 -0700

    Add files via upload

--- RSESpider.py
+++ RSESpider.py
@@ -1,28 +1,51 @@
 import scrapy
 import re
-# Import the CrawlerProcess
-from scrapy.crawler import CrawlerProcess
-import pandas as pd
+from selenium import webdriver
+from scrapy import Selector
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 class RSESpider(scrapy.Spider):
     name = "RSE"
-    start_urls = [
-            'https://www.sciencedirect.com/journal/remote-sensing-of-environment/issues'
-        ]
-    def parse(self, response):
-        urls = response.xpath('//a[@class="anchor text-m"]/@href').extract()    # find all sub urls
-        for url in urls:
-            yield response.follow(url, callback=self.parse_info)
+    urls = []
+    for i in range(1, 4):
+        urls.append('https://www.sciencedirect.com/journal/remote-sensing-of-environment/issues?page={}'.format(i))
+    start_urls = urls
     
-    def parse_info(self, response):  
-        rse_df=[]  
-        if response.xpath('//dd[@class="js-article-author-list u-clr-grey8 text-s"]/text()') is not None:
-            rse_titles = response.xpath('//span[@class="js-article-title"]/text()').extract()
-            rse_authors = response.xpath('//div[@class="text-s u-clr-grey8 js-article__item__authors"]/text()').extract()
-            rse_year = response.xpath('//h3[@class="js-issue-status text-s"]/text()').extract()
-            for rse_title, rse_author in zip(rse_titles, rse_authors):
-                rse_df2 = rse_df.append(pd.DataFrame(list(zip(rse_title,rse_author)), columns=['tilte', 'author']))
-            yield rse_df2
+    def parse(self, response):
+        driver = webdriver.Firefox()
+        driver.get(response.url)
+        WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='0-accordion-tab-1']")))
+        if response.url != 'https://www.sciencedirect.com/journal/remote-sensing-of-environment/issues?page=3':
+            for i in range(19, 0, -1):
+                click_button = "(.//*[normalize-space(text()) and normalize-space(.)='Previous'])[1]/preceding::span[{}]".format(i)
+                driver.find_element_by_xpath(click_button).click()
+            # Wait as long as required, or maximum of 5 sec for element to appear
+            # If successful, retrieves the element
+        else:
+            for i in range(8, 0, -1):
+                click_button = "(.//*[normalize-space(text()) and normalize-space(.)='Previous'])[1]/preceding::span[{}]".format(i)
+                driver.find_element_by_xpath(click_button).click()
 
+        html = driver.page_source 
+        sel = Selector(text=html)
+        driver.quit()
+
+        links = sel.xpath('//a[@class="anchor text-m"]/@href').extract() 
+
+        for link in links:
+            yield response.follow(link, self.parse_info)
+    
+    def parse_info(self, response):
+        rse_authors = response.xpath('//div[@class="text-s u-clr-grey8 js-article__item__authors"]/text()').extract()
+        rse_year = response.xpath('//h3[@class="js-issue-status text-s"]/text()').extract()
+        
+        for author in rse_authors:   
+            yield {
+            'author': author,
+            'year': rse_year
+            }
 
 # run in terminal: scrapy runspider rse/spiders/RSESpider.py  -o res.csv
 
